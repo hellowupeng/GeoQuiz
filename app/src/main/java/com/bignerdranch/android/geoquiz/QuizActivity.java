74797fd55735be33adb668a56da43981ef7f32af
hellowupeng <hellowupeng@gmail.com>
2017-04-29 10:50:50 +0800

    修改toast位置

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -21,14 +22,18 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                Toast toast = Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0, 100);
+                toast.show();
             }
         });
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_totast, Toast.LENGTH_LONG).show();
+                Toast toast = Toast.makeText(QuizActivity.this, R.string.incorrect_totast, Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.LEFT, 0, 0);
+                toast.show();
             }
         });
     }
